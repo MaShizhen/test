ef0b2768b360525a8074a43597f7d6262f82aaf5
taoqf <tao_qiufeng@126.com>
2017-09-25 11:28:46 +0800

    add dependence

--- src/add.ts
+++ src/add.ts
@@ -7,3 +7,11 @@ export function add2(a: number) {
 		return a + b;
 	}
 }
+
+export function add3(a: number, b: number) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(a + b);
+		}, 3000);
+	});
+}
--- tests/add.test.js
+++ tests/add.test.js
@@ -2,6 +2,6 @@ import test from 'ava';
 
 import { add1, add2 } from '../dist/add';
 
-test('add a to b', function (t) {
+test('add a to b', async (t) => {
 	t.is(add1(1, 2), 3);
 });
