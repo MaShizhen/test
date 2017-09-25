cb8060c0495c0141311c19f68add8d3e95ba1437
taoqf <tao_qiufeng@126.com>
2017-09-25 18:16:45 +0800

    add function `add`

--- src/add.ts
+++ src/add.ts
@@ -15,3 +15,44 @@ export function add3(a: number, b: number) {
 		}, 3000);
 	});
 }
+
+export function add4(a: number) {
+	return function (b: number) {
+		return a + b;
+	}
+}
+
+export function add5(a: number) {
+	return (b: number) => {
+		return a + b;
+	}
+}
+
+export function add6(a: number, b: number) {
+	class Add {
+		public static add(a: number, b: number) {
+			return a + b;
+		}
+	}
+	return Add.add(a, b);
+}
+
+export function add7(a: number, b: number) {
+	class Add {
+		public add(a: number, b: number) {
+			return a + b;
+		}
+	}
+	const e = new Add();
+	return e.add(a, b);
+}
+
+// add8 will work in js, too.
+// export function add8(a: number, b: number) {
+// 	function Add() { }
+// 	Add.prototype.add = function (a: number, b: number) {
+// 		return a + b;
+// 	};
+// 	const e = new Add();
+// 	return e.add(a, b);
+// }
